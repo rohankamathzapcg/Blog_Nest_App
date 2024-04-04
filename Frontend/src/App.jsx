3fe724d8638927441a7d937d0ad3a63ef3ad247a
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-04-04 17:49:28 +0530

    Evening Session

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,7 @@ import CategoryList from "./Pages/Categories/CategoryList";
 import AddBlogPost from "./Pages/BlogPost/AddBlogPost";
 import EditBlogPost from "./Pages/BlogPost/EditBlogPost";
 import BlogPost from "./Pages/BlogPost/BlogPosts";
+import HomePage from "./Pages/HomePage";
 
 const App = () => {
   return (
@@ -17,7 +18,8 @@ const App = () => {
         <Navbar />
 
         <Routes>
-          
+
+          <Route path='/' element={<HomePage />} />
           <Route path="/admin/categorylist" element={<CategoryList />} />
           <Route path="/add-blogs" element={<AddBlogPost />} />
           <Route path="/edit-blogs" element={<EditBlogPost />} />
--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -1,28 +1,50 @@
-
+import { useEffect, useState } from "react";
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
-  return (
-    <>
-      <div className='container'>
-        <div className='my-5'>
-            <div className="row align-items-lg-stretch">
-                <div className="col-12 col-md-4 mb-2">
-                    <div className="card h-100">
-                        <img className="card-img-top" src="" alt="" />
-                        <div className="card-body d-flex flex-column">
-                            <h5 className="card-title"></h5>
-                            <p className="card-text"></p>
-                            <span className="d-flex align-items-end" style>
-                                <a href="">Read More.</a>
-                            </span>
-                        </div>
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(() => {
+        axios.get(`${apiUrl}/api/BlogPost`)
+            .then((result) => {
+                setBlogs(result.data)
+            })
+            .catch(err => console.log(err))
+    }, [apiUrl])
+    
+    return (
+        <>
+            <div className='container'>
+                <div className='my-5'>
+                    <div className="row align-items-lg-stretch">
+                        {
+                            blogs.map((blog, index) => {
+                                return (
+                                    <div key={index} className="col-12 col-md-4 mb-3">
+                                        <div className="card h-100">
+                                            {/* Center the image */}
+                                            <img className="card-img-top mx-auto p-2" src={blog.featuredImageUrl} alt="" style={{ width: "80px", height: "80px" }} />
+                                            <div className="card-body text-center d-flex flex-column">
+                                                <h3 className="card-title">{blog.title}</h3>
+                                                <p className="card-text">{blog.description}</p>
+                                                <span className="d-flex align-items-end" style={{ flex: "1" }}>
+                                                    <Link className="btn btn-dark" to={blog.urlHandle}>Read More</Link>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+
                     </div>
                 </div>
             </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default HomePage;
